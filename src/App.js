453fbe0fa4679acf6b621bcfcefd19c50efcc9a2
George <george@wizardofweb.com>
2022-02-12 13:41:37 +0800

    Added: NavList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +10,15 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          ></Route>
         </Routes>
       </Router>
     </div>
